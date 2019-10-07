e687ba4bd7a9ed9cfdd0128af0751fc12ec6c7d7
dare2python <31766642+Dare2python@users.noreply.github.com>
2019-10-07 22:34:01 +0300

    matrix view and the app state

--- src/view/Matrix.js
+++ src/view/Matrix.js
@@ -1,10 +1,25 @@
 var m = require("mithril")
 
+
+const INITIAL_ARR = [
+    [[1,0], [1,0], [1,0], [0,0], [0,0], [0,0]],
+    [[0,0], [1,0], [0,0], [0,0], [0,0], [0,0]],
+    [[1,0], [1,0], [1,0], [0,0], [0,0], [0,0]],
+    [[0,0], [0,0], [2,0], [4,0], [4,0], [0,0]],
+    [[0,0], [0,0], [0,0], [2,0], [0,0], [0,0]],
+    [[0,0], [0,0], [1,0], [2,0], [4,0], [0,0]]
+];
+
+const state = {
+    arr: JSON.parse(JSON.stringify(INITIAL_ARR)),
+    hsum: 0
+};
+
 module.exports = {
-    view: function() {
-        return m("main", [
-            m("h1", {class: "title"}, "You are in the Matrix"),
-            m("button", "A button"),
+    view: () =>
+        m("main", [
+            m("h1", {class: "title"}, "the hourglass"),
+            m("p", state.arr),
+            m("h4", `Hourglass Sum = ${state.hsum}`),
         ])
-    }
 }
